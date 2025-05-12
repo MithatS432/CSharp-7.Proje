d302638bc99d05ae0625a38c55cd84fa7214fa64
Mithat <mithatsirmali@gmail.com>
2025-05-12 15:57:35 +0300

    Add files via upload
    Dersimiz Eklendi

---
+++ Karar Yapıları1/Karar Yapıları1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Karar_Yapıları1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Notunuzu giriniz (0-100): ");
+            int not = int.Parse(Console.ReadLine());
+            if(not<0 || not>100)
+            {
+                Console.WriteLine("Lütfen 0-100 arasında bir not giriniz.");
+            }
+            else if (not >= 90)
+            {
+                Console.WriteLine("Harf Notu: AA");
+            }
+            else if (not >= 80)
+            {
+                Console.WriteLine("Harf Notu: BA");
+            }
+            else if (not >= 70)
+            {
+                Console.WriteLine("Harf Notu: BB");
+            }
+            else if (not >= 60)
+            {
+                Console.WriteLine("Harf Notu: CB");
+            }
+            else if (not >= 50)
+            {
+                Console.WriteLine("Harf Notu: CC");
+            }
+            else
+            {
+                Console.WriteLine("Harf Notu: FF (Kaldınız)");
+            }
+        }
+    }
+}
